8615392bef5ebafb1222383a8f5b6d86eccd090b
alpha1025 <66642917+alpha1025@users.noreply.github.com>
2020-06-30 01:08:19 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,7 @@ client.on('ready', () => {
 console.log('Bot: Hosting ' + `${client.users.size}` + ' users, in ' + `${client.channels.size}` + ' channels of ' + `${client.guilds.size}` + ' guilds.');
     client.user.setStatus('online')
     client.user.setPresence({ game: { name: 'ALPHA' , type: 'WATCHING' }, status: 'idle' })
-    .then(console.log)
-    .catch(console.error);
+
 
 client.on('message', message => {
     if (message.content === 'ping') {
