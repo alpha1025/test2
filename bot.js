829c505a17f350ebb31fa2006417c717afc5b6c9
alpha1025 <66642917+alpha1025@users.noreply.github.com>
2020-06-30 00:23:57 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+client.on('ready',() =>
+  {
+    console.log ('connected as '+ client.user.tag)
+    client.user.setActivity("GTA V",{type:"WATCHING"})
+  })
+  client.on('message',message => {
+    if(message.content === '!ping')
+    {
+      message.channel.send('Pong.')
+    }
+  })
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
