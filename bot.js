c74ce68b4c59499590fcdd2e6ec853bf29680ee2
alpha1025 <66642917+alpha1025@users.noreply.github.com>
2020-06-30 01:35:17 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,23 +5,12 @@ const client = new Discord.Client();
 client.on('ready', () => {
 console.log('Bot: Hosting ' + `${client.users.size}` + ' users, in ' + `${client.channels.size}` + ' channels of ' + `${client.guilds.size}` + ' guilds.');
     client.user.setStatus('online')
+   client.user.setActivity("TV", {type: "WATCHING"})
     
-    client.user.setPresence({
-        game: {
-            name: ' Grand Theft Auto V',
-            type: "Playing",
-            url: "https://discordapp.com/"
-        }
-    });
-});
+    
+   
     
 
-client.on('message', message => {
-     if (receivedMessage.author == client.user) {
-        return }
-    else{        message.reply('i am bot);
-                               }
-});
 
 
 // THIS  MUST  BE  THIS  WAY
