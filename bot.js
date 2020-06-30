fabb64741d3009c40509f65321653b934cb8b207
alpha1025 <66642917+alpha1025@users.noreply.github.com>
2020-06-30 13:16:04 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('ready', () => {
-    console.log('Delpoyed Succesfully!');
-    client.user.setStatus('undercover')
-    client.user.setPresence({
-        game: {
-            name: 'Grand theft auto V',
-            type: "Playing",
-            url: "https://discordapp.com/"
-        }
-    });
-});
-client.on('message', (receivedMessage) => {
-    // Prevent bot from responding to its own messages
-    if (receivedMessage.author == client.user) {
-        return
-    }
 
-    receivedMessage.channel.send({embed: {
-  color: 3447003,
-  description: "I am bot under development!"
-  
-}})
-})
+
+client.on('ready', () => {
+console.log('Bot: Hosting ' + `${client.users.size}` + ' users, in ' + `${client.channels.size}` + ' channels of ' + `${client.guilds.size}` + ' guilds.');
+    client.user.setStatus('online')
+   client.user.setActivity("TV", {type: "WATCHING"})
+
+
+
+
+
+    console.log('I am ready!');
+    client.user.setActivity("GTA V",{type:"Playing"})
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN),
+client.login(process.env.BOT_TOKEN);
