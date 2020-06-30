3e3f14932d0044272e050636a832de039a004809
alpha1025 <66642917+alpha1025@users.noreply.github.com>
2020-06-30 11:03:08 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
 client.on('ready', () => {
-console.log('Bot: Hosting ' + `${client.users.size}` + ' users, in ' + `${client.channels.size}` + ' channels of ' + `${client.guilds.size}` + ' guilds.');
-    client.user.setStatus('online')
-   client.user.setActivity("TV", {type: "WATCHING"})
-    
-    
-   
-    
+    console.log('I am ready!');
+    client.user.setActivity("GTA V",{type:"Playing"})
+});
 
-
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
