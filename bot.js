0661fd8053de7f028aaabe7d48cf49a289ebf59f
alpha1025 <66642917+alpha1025@users.noreply.github.com>
2020-06-30 20:39:47 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('Delpoyed Succesfully!');
   
-    
-    client.user.setPresence({ game: { name: 'GTA V' , type: 'WATCHING' }, status: 'idle' })
+     
+    client.user.setPresence({ game: { name: 'GTA V' , type: 'Playing' }, status: 'idle' })
     
     
 });
