3e58d7b96fadcff37d3c1c136e7faa4ab3699ac3
alpha1025 <66642917+alpha1025@users.noreply.github.com>
2020-07-03 00:48:42 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
     console.log('Delpoyed Succesfully!');
@@ -34,48 +34,5 @@ client.on('ready', () => {
 
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
-
-const client = new Discord.Client();
-client.on('ready', () => {
-    console.log('Delpoyed Succesfully!');
-    console.log("Servers:")
-    client.guilds.forEach((guild) => {
-        console.log(" - " + guild.name)
-        // List all channels
-        guild.channels.forEach((channel) => {
-            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
-        })
-    })
-    var generalChannel = client.channels.get("727732332253151242") // Replace with known channel ID
-    generalChannel.send({embed:{
-        color: 43127
-        ,description: "Bot is deployed " ,
-	 timestamp: new Date(),
-    
-	footer: {
-		text: 'Deployed at ',
-		icon_url: 'https://i.imgur.com/TV9NcbJ.jpg',
-		timestamp: new Date(),
-		text: "by alpha",
-		
-	},
-    }})
-  
-   client.user.setPresence({
-        game: {
-            name: 'LoveDarsh😍🛐',
-            type: "Watching",
-            url: "https://www.youtube.com/"
-        }, status: "online"
-    });
-   
-});
-
-
-
-
-})
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
