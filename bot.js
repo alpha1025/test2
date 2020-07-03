7e9b3fead73e869fd47c330936a0134933cc04f5
alpha1025 <66642917+alpha1025@users.noreply.github.com>
2020-07-03 00:46:47 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,42 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js');const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+    console.log('Delpoyed Succesfully!');
+    console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        console.log(" - " + guild.name)
+        // List all channels
+        guild.channels.forEach((channel) => {
+            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+        })
+    })
+    var generalChannel = client.channels.get("727732332253151242") // Replace with known channel ID
+    generalChannel.send({embed:{
+        color: 43127
+        ,description: "Bot is ready" ,
+	 timestamp: new Date(),
+
+	footer: {
+		text: 'Developed by Alpha',
+		icon_url: 'https://i.imgur.com/TV9NcbJ.jpg',
+		timestamp: new Date(),
+
+	},
+    }})
+
+   client.user.setPresence({
+        game: {
+            name: 'LoveDarsh😍🛐',
+            type: "Watching",
+            url: "https://www.youtube.com/"
+        }, status: "online"
+    });
+
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
+
 const client = new Discord.Client();
 client.on('ready', () => {
     console.log('Delpoyed Succesfully!');
